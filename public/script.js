6e2306a7fea54b359e698362cec1af8cee98b00b
seanbuenavista <b2029sslbuenavista@pshs.edu.ph>
2025-08-07 10:12:32 +0800

    code

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
 // no script tag needed
+let age;
+age=14
+let maxAge;
+maxAge=100
+let numPerDay;
+numPerDay=1
+let totalRequired
+totalRequired=(numPerDay*365)*(maxAge-age)
+document.write("You will need $totalRequired to last until the ripe old age of $maxAge")
