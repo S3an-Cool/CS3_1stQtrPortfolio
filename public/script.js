d952c411f765f64e5a61b91f9fc327df606e7223
seanbuenavista <b2029sslbuenavista@pshs.edu.ph>
2025-08-07 10:18:17 +0800

    code

--- public/script.js
+++ public/script.js
@@ -7,4 +7,4 @@ let numPerDay;
 numPerDay=1
 let totalRequired
 totalRequired=(numPerDay*365)*(maxAge-age)
-document.write("You will need $totalRequired to last until the ripe old age of $maxAge")
+document.write(`You will need ${totalRequired} to last until the ripe old age of ${maxAge}`)
